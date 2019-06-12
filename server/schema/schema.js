77cacade65be1747449d40c2d88aaf85d299bcf6
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-06-12 13:20:13 -0400

    adding schema for sales

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,2 +1,44 @@
 const graphql = require('graphql');
 const _ = require('lodash');
+
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphql;
+
+const StickType = new GraphQLObjectType({
+    name: "Stick",
+    fields: () => ({
+        id: { type: GraphQLID },
+        brand: { type: GraphQLString },
+        material: { type: GraphQLString },
+        model: { type: GraphQLString },
+        hand: { type: GraphQLString },
+        length: { type: GraphQLInt },
+        curve: { type: GraphQLInt },
+        pattern: { type: GraphQLString },
+        color: { type: GraphQLString },
+        condition: { type: GraphQLString },
+        price: { type: GraphQLInt }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        stick: {
+            type: StickType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                //get data from db
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
